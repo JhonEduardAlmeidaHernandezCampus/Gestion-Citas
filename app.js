9658652a4f803237cad6a6127b6e2268084fdd24
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-07-12 15:11:58 -0500

    feat: :memo: EndPoint Get Estado Cita
    Se agrega el EndPoint Get  Estado de Cita en donde trae todos los datos que hay en dicha tabla
    
    by: Jhon Hernandez

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ import storageCita from './routers/cita.js';
 import storageTipoDocumento from './routers/tipo_documento.js';
 import storageGenero from './routers/genero.js';
 import storageAcudiente from './routers/acudiente.js';
+import storageEstadoCita from './routers/estado_cita.js';
 
 dotenv.config();
 let appExpress = express();
@@ -15,6 +16,7 @@ appExpress.use("/cita", storageCita);
 appExpress.use("/tipo_documento", storageTipoDocumento);
 appExpress.use("/genero", storageGenero);
 appExpress.use("/acudiente", storageAcudiente);
+appExpress.use("/estado_cita", storageEstadoCita);
 
 let config = JSON.parse(process.env.MY_CONFIG)
 appExpress.listen(config, () => console.log(`http://${config.hostname}:${config.port}`))
---
+++ routers/estado_cita.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv';
+import { Router } from 'express';
+import mysql from 'mysql2';
+
+let storageEstadoCita = Router();
+dotenv.config();
+
+let con = undefined;
+
+storageEstadoCita.use((req, res, next) => {
+    let connect = JSON.parse(process.env.MY_CONNECT)
+    con = mysql.createPool(connect);
+    next();
+})
+
+/* http://127.11.11.11:5010/usuario/ */
+storageEstadoCita.get("/", (req, res)=>{
+    con.query(
+    /*sql*/`SELECT * FROM estado_cita`,
+    (err, data, fil) => {
+        res.send(JSON.stringify(data))
+    }) 
+})
+
+export default storageEstadoCita;
