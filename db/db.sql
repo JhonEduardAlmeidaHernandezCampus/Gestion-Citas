e506c9d49b3d507ea783a2cafde5eeccc70be9e0
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-07-12 14:45:50 -0500

    docs: :card_file_box: Database Gestion
    Se corrigion la base de datos y se le agrego nuevos datos para insertar
    
    Jhon Hernandez

--- db/db.sql
+++ db/db.sql
@@ -2,7 +2,6 @@ SHOW DATABASES;
 CREATE DATABASE gestion_citas;
 USE gestion_citas;
 
-DROP DATABASE gestion_citas;
 
 
 CREATE TABLE usuario(
@@ -11,6 +10,7 @@ CREATE TABLE usuario(
     usu_segdo_nombre VARCHAR(45) NOT NULL,
     usu_primer_apellido_usuar VARCHAR(50) NOT NULL,
     usu_segdo_apellido_usuar VARCHAR(50),
+    usu_edad INT NOT NULL,
     usu_telefono VARCHAR(50) NOT NULL,
     usu_direccion VARCHAR(100) NOT NULL,
     usu_email VARCHAR(100) NOT NULL,
@@ -31,8 +31,7 @@ CREATE TABLE genero(
     gen_abreviatura VARCHAR(20) NOT NULL
 );
 
-CREATE TABLE acudiente(MY_CONFIG={"hostname":"127.10.10.10", "port":5100}
-MY_CONNECT={"host":"localhost","user":"campus","password":"campus2023","database":"db_mer_citas_medicas", "port":3306}
+CREATE TABLE acudiente(
     acu_codigo INT NOT NULL PRIMARY KEY,
     acu_nombreCompleto VARCHAR(100) NOT NULL,
     acu_telefono VARCHAR(100) NOT NULL,
@@ -82,15 +81,66 @@ ALTER TABLE medico ADD CONSTRAINT fk_medico_consultorio FOREIGN KEY(med_consulto
 
 /* INSERT DATOS */
 
-INSERT INTO tipo_documento (tipdoc_nombre, tipdoc_abreviatura) VALUES ("Cedula", "CC");
-INSERT INTO genero (gen_nombre, gen_abreviatura) VALUES ("Mujer", "M");
-INSERT INTO acudiente (acu_codigo, acu_nombreCompleto, acu_telefono, acu_direccion) VALUES (28224423, "Carmen Hernandez", "+57 300421389", "Calle 11B Torres Zafiro");
-INSERT INTO usuario (usu_id, usu_nombre, usu_segdo_nombre, usu_primer_apellido_usuar, usu_segdo_apellido_usuar, usu_telefono, usu_direccion, usu_email, usu_tipodoc, usu_genero, usu_acudiente) VALUES (1231312, "Camila", "Alejandra", "Martinez", "Sanchez", "+57 123123214", "Calle 11B", "JhonHernandez.1899@gmail.com", 1, 2, 28224423);
-INSERT INTO estado_cita (estcita_nombre) VALUES ("Rechazado");
-INSERT INTO especialidad (esp_nombre) VALUES ("Medico Urologo");
-INSERT INTO consultorio (cons_codigo, cons_nombre) VALUES (4, "Consultorio General");
-INSERT INTO medico (med_nroMatriculaProsional, med_nombreCompleto, med_consultorio, med_especialidad) VALUES (1234567, "Jhon Hernandez", 4, 2);
-INSERT INTO cita (cit_fecha, cit_estadoCita, cit_medico, cit_datosUsuario) VALUES ("2023-02-15", 3, 1234567, 1231312);
+INSERT INTO tipo_documento (tipdoc_nombre, tipdoc_abreviatura) VALUES 
+("Cedula", "CC"),
+("Targeta", "TI"),
+("Pasaporte", "PT"),
+("Registro Civil", "RC");
+
+INSERT INTO genero (gen_nombre, gen_abreviatura) VALUES 
+("Hombre", "H"),
+("Mujer", "M"),
+("Transgénero", "T"),
+("Intersexual", "I"),
+("Queer", "Q");
+
+INSERT INTO acudiente (acu_codigo, acu_nombreCompleto, acu_telefono, acu_direccion) VALUES 
+(12345, "Jesus Cardona", "+57 13214124", "123 Calle Principal"),
+(67890, "Marcelo Luz", "+57 412314124", "Apartamento 456 Avenida del Sol"),
+(09876, "Zaira Moran", "+57 51321312", "789 Rua das Flores"),
+(54321, "Eduardo Sabater", "+57 12321323", "321 High Street Town XYZ"),
+(13579, "Ariel Lledo", "+57 312314123", "Appartement 789");
+
+INSERT INTO usuario (usu_id, usu_nombre, usu_segdo_nombre, usu_primer_apellido_usuar, usu_segdo_apellido_usuar, usu_telefono, usu_direccion, usu_email, usu_tipodoc, usu_genero, usu_acudiente) VALUES 
+(12345, "Sofia", "Andrea", "Johnson", "Sanchez", "+57 123213", "Calle 45", "Sofia@gmail.com", 1, 1, 12345),
+(54321, "Liam", "Camila", "Rodriguez", "Perez", "+57 51512213", "Calle 21", "Liam@gmail.com", 2, 2, 67890),
+(67890, "Ethan", "Camilo", "Martinez", "Hernandez", "+57 31123213", "Calle 32", "Ethan@gmail.com", 3, 3, 09876),
+(09876, "Ava", "Alejandra", "Thompson", "Herrera", "+57 31451512", "Calle 64", "Ava@gmail.com", 4, 4, 54321),
+(13579, "Mia", "Fernanda", "Smith", "Rueda", "+57 214315125", "Calle 97", "Mia@gmail.com", 1, 5, 13579);
+
+INSERT INTO estado_cita (estcita_nombre) VALUES 
+("Activo"),
+("Aprobado"),
+("Rechazado"),
+("En Espera"),
+("Completado");
+
+INSERT INTO especialidad (esp_nombre) VALUES 
+("Medico Cardiologo"),
+("Medico Dermatologo"),
+("Medico Ginecologo"),
+("Medico Neurologo"),
+("Medico Ortopedista");
+
+INSERT INTO consultorio (cons_codigo, cons_nombre) VALUES 
+(1, "Consultorio Cardiología"),
+(2, "Consultorio Dermatología"),
+(3, "Consultorio Ginecología"),
+(4, "Consultorio Neurología"),
+(5, "Consultorio Ortopedia");
+
+INSERT INTO medico (med_nroMatriculaProsional, med_nombreCompleto, med_consultorio, med_especialidad) VALUES 
+(12345, "Juan Garcia", 1, 1),
+(67890, "María Rodriguez", 2, 2),
+(09876, "Carlos Lopez", 3, 3),
+(54321, "Ana Martinez", 4, 4),
+(13579, "Laura Sanchez", 5, 5);
+INSERT INTO cita (cit_fecha, cit_estadoCita, cit_medico, cit_datosUsuario) VALUES 
+("2023-02-13", 1, 12345, 12345),
+("2023-02-14", 2, 67890, 54321),
+("2023-02-15", 3, 09876, 67890),
+("2023-02-16", 4, 54321, 09876),
+("2023-02-17", 5, 13579, 13579);
 
 
 
