f82fe49afbe7069ea787dd6087868d6c090451c0
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-07-12 21:02:29 -0500

    feat: :memo: Consulta 10
    Consulta 10, Obtener los consultorio donde se aplicó las citas de un paciente
    
    by: Jhon Hernandez

--- routers/consultas/consultaConsultorias.js
+++ routers/consultas/consultaConsultorias.js
@@ -23,4 +23,14 @@ storageConsultaConsultorias.get("/consultorias/:usu_id", (req, res)=>{
     }) 
 })
 
+/* http://127.10.10.10:5010/consultarConsultorias/consultorios/13579 */
+storageConsultaConsultorias.get("/consultorios/:usu_id", (req, res)=>{
+    con.query(
+    /*sql*/`SELECT usu_id, usuario.usu_nombre, usuario.usu_primer_apellido_usuar, cita.cit_fecha, medico.med_nombreCompleto, consultorio.cons_nombre FROM usuario INNER JOIN cita ON usuario.usu_id = cita.cit_datosUsuario INNER JOIN medico ON cita.cit_medico = medico.med_nroMatriculaProsional INNER JOIN consultorio ON medico.med_consultorio = consultorio.cons_codigo WHERE usu_id = ?`,
+    [req.params.usu_id],
+    (err, data, fil) => {
+        res.send(JSON.stringify(data))
+    }) 
+})
+
 export default storageConsultaConsultorias;
