423d3f0d6700d02b23a91e157fe6603d207215c8
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-07-12 20:03:48 -0500

    feat: :memo: Consulta 7
    Consulta 7, Citas para un dia especifico
    
    by: Jhon Hernandez

--- routers/consultas/consultaCitas.js
+++ routers/consultas/consultaCitas.js
@@ -22,11 +22,21 @@ storageConsultaCitas.get("/", (req, res)=>{
     }) 
 })
 
-/* http://127.11.11.11:5010/consultarCitas/ */
+/* http://127.10.10.10:5010/consultarCitas/proximaCita/13579 */
 storageConsultaCitas.get("/proximaCita/:usu_id", (req, res)=>{
     con.query(
     /*sql*/`SELECT usu_id, usuario.usu_nombre, usuario.usu_primer_apellido_usuar, usuario.usu_telefono, cita.cit_fecha FROM usuario INNER JOIN cita ON usuario.usu_id = cita.cit_datosUsuario WHERE usu_id = ? ORDER BY cita.cit_fecha DESC`,
     [req.params.usu_id],
+    (err, data, fil) => {
+        res.send(JSON.stringify(data))
+    }) 
+})
+
+/* http://127.10.10.10:5010/consultarCitas/citaDia/2023-02-18 */
+storageConsultaCitas.get("/citaDia/:cit_fecha", (req, res)=>{
+    con.query(
+    /*sql*/`SELECT * FROM cita WHERE cit_fecha = ?`,
+    [req.params.cit_fecha],
     (err, data, fil) => {
         res.send(JSON.stringify(data))
     }) 
